a8fa86ba5458403476ebb57d9ea7e3d0692eba40
divesh <rajpootdivesh5@gmail.com>
2025-02-20 18:05:57 +0530

    added cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,17 @@ dotenv.config();
 const routes = require("./routes")
 const connectDB = require('./config/db');
 const path = require("path")
+const cors = require("cors")
 
 
 const app = express();
 connectDB();
 
 app.use(express.json());
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://smart-task-frontend-tp1d.vercel.app/'],
+    credentials: true
+  }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use("/api/users", routes.userRoutes)
